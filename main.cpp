b3802f28fb86b3647d36499aaf35e738903d5a65
denis-beqiraj <114519777+denis-beqiraj@users.noreply.github.com>
2023-03-02 16:44:39 +0100

    Added comments

--- main.cpp
+++ main.cpp
@@ -3,13 +3,17 @@
 #include "include/Error.h"
 
 int main(int args,char** argv){
+    //API token as argument
     OpenAI::ChatGPT chatGpt{argv[1]};
     try {
+        //type of user and the message to ask
         auto response = chatGpt.sendRequest("user","Give me 5 words");
+        //Iterate all answers
         for(const auto& choice:response.choices){
             std::cout<<choice.message.content;
         }
     }catch(OpenAI::Error& e){
+        //JSON error returned by the server
         std::cout<<e.what();
     }
 }
