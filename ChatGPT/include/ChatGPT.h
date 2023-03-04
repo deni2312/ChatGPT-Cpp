256a037b6adf1a05b93e60c871c8f745aa56eb9a
denis-beqiraj <114519777+denis-beqiraj@users.noreply.github.com>
2023-03-04 19:06:14 +0100

    Added whisper

--- ChatGPT/include/ChatGPT.h
+++ ChatGPT/include/ChatGPT.h
@@ -4,7 +4,8 @@ namespace OpenAI {
     class ChatGPT {
     public:
         explicit ChatGPT(const std::string& token);
-        OpenAI::ChatCompletion sendRequest(const std::string& role,const std::string& content);
+        OpenAI::ChatCompletion askChatGPT(const std::string& role, const std::string& content);
+        std::string askWhisper(const std::string& audio_path);
     private:
         std::string m_token;
         std::string m_link;
--- ChatGPT/src/ChatGPT.cpp
+++ ChatGPT/src/ChatGPT.cpp
@@ -8,7 +8,7 @@ OpenAI::ChatGPT::ChatGPT(const std::string& token):m_token{token},m_link{"https:
 
 }
 
-OpenAI::ChatCompletion OpenAI::ChatGPT::sendRequest(const std::string& role,const std::string& content) {
+OpenAI::ChatCompletion OpenAI::ChatGPT::askChatGPT(const std::string& role, const std::string& content) {
     auto json="{\n"
                     "  \"model\": \"gpt-3.5-turbo\",\n"
                     "  \"messages\": [{\"role\": \""+role+"\", \"content\": \""+content+"\"}]\n"
@@ -22,4 +22,25 @@ OpenAI::ChatCompletion OpenAI::ChatGPT::sendRequest(const std::string& role,cons
         throw OpenAI::Error{j.dump()};
     }
     return chatCompletion;
+}
+
+std::string OpenAI::ChatGPT::askWhisper(const std::string &audio_path) {
+    std::string url = "https://api.openai.com/v1/audio/transcriptions";
+    std::string model_name = "whisper-1";
+    std::string auth_token = "Bearer " + std::string(m_token);
+
+    cpr::Multipart multipart_data = {
+            {"model", model_name},
+            {"file", cpr::File{audio_path}}
+    };
+
+    auto r = cpr::Post(cpr::Url{url},
+                                cpr::Header{{"Authorization", auth_token},
+                                            {"Content-Type", "multipart/form-data"}},
+                                multipart_data).text;
+    nlohmann::json j=nlohmann::json::parse(r);
+    if(j.contains("error")) {
+        throw OpenAI::Error{j.dump()};
+    }
+    return j["text"];
 }
